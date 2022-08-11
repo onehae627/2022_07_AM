6f5eba615900f6c3f831267b2dcffb5f97943cae
onehae627 <onehae627@gmail.com>
2022-08-11 15:13:42 +0900

    게시물 삭제 기능구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -81,28 +81,26 @@ public class Main {
 
 				int id = Integer.parseInt(cmdBits[2]);
 
-				Article foundArticle = null;
+				int foundIndex = -1;
 
 				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
 
 					if (article.id == id) {
 
-						foundArticle = article;
+						foundIndex = i;
 						break;
 					}
 				}
 
-				if (foundArticle == null) {
+				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
-				articles.remove(id -1);
+				articles.remove(foundIndex);
 				System.out.printf("%d번 글을 삭제했습니다.\n", id);
-				
 
-			}
-				else {
+			} else {
 				System.out.println("존재하지않습니다.");
 			}
 
